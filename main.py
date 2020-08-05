176be06dbffd38436c838d1a20e994389a66b1d4
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-05 11:56:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import docxModule
+import scrapeModule
+
+print("Enter an essay topic")
+topic = input("--> ")
+docxModule.title(topic)
+scrapeModule.test()
+
+#a = doc.paragraphs[0].text
+#b = doc.paragraphs[1].text
+#print(doc.paragraphs[1].runs[0].text)
+#print(a + b)
+
+
+
