d6e624e46e5da26ba24063932aeccdd7e673b684
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-05 11:57:06 -0700

    Update docxModule.py

--- docxModule.py
+++ docxModule.py
@@ -1,1 +1,34 @@
+import docx
+
+def title(topic):
+    doc = docx.Document('Document.docx')
+    doc.add_paragraph(topic)
+    doc.save("Document.docx")
+    return "1"
+
+
+def newPara():
+    doc = docx.Document('Document.docx')
+    doc.save("Document.docx")
+    return "2"
+
+
+def addPara():
+    doc = docx.Document('Document.docx')
+    doc.save("Document.docx")
+    return "3"
+
+
+def fontSize():
+    doc = docx.Document('Document.docx')
+    doc.save("Document.docx")
+    return "4"
+
+
+def paraSpace():
+    doc = docx.Document('Document.docx')
+    doc.save("Document.docx")
+    return "5"
+
+
 
