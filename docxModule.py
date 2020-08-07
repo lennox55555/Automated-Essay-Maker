e713d20ea0c20a633c190c77d9e3a63f93c863b5
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-06 20:56:54 -0700

    Update docxModule.py

--- docxModule.py
+++ docxModule.py
@@ -1,26 +1,40 @@
 import docx
+from docx import Document, document
+from docx.shared import Pt
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
-def title(topic):
+
+
+def title(title):
     doc = docx.Document('Document.docx')
-    doc.add_paragraph(topic)
-    doc.save("Document.docx")
+    doc.add_paragraph(title)
+    font.size = Pt(12)
+    font.name = 'Times New Roman'
+    document = Document('Document.docx')
+    paragraph = document.add_paragraph()
+    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    doc.save('Document.docx')
     return "1"
 
 
 def newPara():
     doc = docx.Document('Document.docx')
+    doc.add_paragraph("")
     doc.save("Document.docx")
     return "2"
 
 
-def addPara():
+def addPara(text):
     doc = docx.Document('Document.docx')
+    doc.add_paragraph(text)
     doc.save("Document.docx")
     return "3"
 
 
 def fontSize():
     doc = docx.Document('Document.docx')
+    font.size = Pt(12)
     doc.save("Document.docx")
     return "4"
 
@@ -30,5 +44,8 @@ def paraSpace():
     doc.save("Document.docx")
     return "5"
 
-
-
+def font(font):
+    doc = docx.Document('Document.docx')
+    font.name = "Times New Roman"
+    doc.save("Document.docx")
+    return "6"
