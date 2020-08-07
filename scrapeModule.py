ecd081f7931b16987347ddc4a44fe4d48aa38e1e
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-06 20:57:17 -0700

    Update scrapeModule.py

--- scrapeModule.py
+++ scrapeModule.py
@@ -1,4 +1,11 @@
-#import
+import docxModule
+from bs4 import BeautifulSoup
+import requests
 
-def test():
-    print("Done")
+def scrapeText(file):
+    with open(file) as html_file:
+        soup = BeautifulSoup(html_file, 'lxml')
+    for article in soup.find_all('div', class_='article'):
+        text = article.p.text
+        docxModule.newPara()
+        docxModule.addPara(text)
