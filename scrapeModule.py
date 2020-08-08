c35b0349b587f0c32d86163b03f404f943e28dd7
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-07 18:11:52 -0700

    Update scrapeModule.py

--- scrapeModule.py
+++ scrapeModule.py
@@ -2,10 +2,40 @@ import docxModule
 from bs4 import BeautifulSoup
 import requests
 
-def scrapeText(file):
-    with open(file) as html_file:
-        soup = BeautifulSoup(html_file, 'lxml')
-    for article in soup.find_all('div', class_='article'):
-        text = article.p.text
+
+
+def scrapeResearch(sourceAlpha):
+    # sourceA
+    soupA = BeautifulSoup(sourceAlpha, 'lxml')
+    for articleA in soupA.find_all('article'):
+        summaryA = articleA.find('div', class_='entry-content').p.text
         docxModule.newPara()
-        docxModule.addPara(text)
+        docxModule.addPara(summaryA)
+
+
+def scrapeArgue(sourceAlpha, sourceBravo):
+    # sourceA
+    soupA = BeautifulSoup(sourceAlpha, 'lxml')
+    for articleA in soupA.find_all('article'):
+        summaryA = articleA.find('div', class_='entry-content').p.text
+        docxModule.newPara()
+        docxModule.addPara(summaryA)
+    #sourceB
+    soupB = BeautifulSoup(sourceBravo, 'lxml')
+    for articleB in soupB.find_all('article'):
+        summaryB = articleB.find('div', class_='entry-content').p.text
+        docxModule.newPara()
+        docxModule.addPara(summaryB)
+
+
+def scrapeExpo():
+    print("add expository essay code")
+
+
+def scrapeDescript():
+    print("add descriptive essay code")
+
+def scrapeNarrate():
+    print("add narrative essay code")
+
+
