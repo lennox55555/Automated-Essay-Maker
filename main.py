da06a14d3e96f451d77bc61cdbd3fed103aeeadc
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-07 18:05:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+import requests
 import docxModule
 import scrapeModule
 
-file = 'simple.html'
+sourceAlpha = requests.get('http://coreyms.com').text
+sourceBravo = requests.get('http://coreyms.com').text
 
 print("Enter an essay title")
 title = input("--> ")
 docxModule.title(title)
+print("")
 
-print("Is this a Research Essay or an Argumentative Essay")
+print("What type of essay are you wanting?\n1 = Research\n2 = Argumentative Essays\n3 = Descriptive\n4 = Narrative\n5 = Expository Essay")
 topic = input("-->")
+if (topic == "1"):
+    scrapeModule.scrapeResearch(sourceAlpha)
+elif (topic == "2"):
+    scrapeModule.scrapeArgue(sourceAlpha, sourceBravo)
+else:
+    print("Unknown topic \nError Code A001\n ")
 
 print("Enter desired font")
 font = input("-->")
+print("")
 #docxModule.font(font)
 
 print("Enter font size")
 fontSize = input("-->")
+print("")
 #docxModule.fontSize(fontSize)
 
 print("Enter desired line-spacing")
 lineSpacing = input("-->")
+print("")
 
-scrapeModule.scrapeText(file)
+
 
 
