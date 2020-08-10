5dc5e1135ced9f9a798b0be914a16e725503668b
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-09 17:35:45 -0700

    Create searchModule.py

---
+++ searchModule.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+import pyperclip
+
+def openWiki():
+    pyautogui.press('win')
+    pyautogui.typewrite("https://www.wikipedia.org/")
+    pyautogui.typewrite(["enter"])
+
+def newTab():
+    pyautogui.hotkey('ctrl', 't')
+    pyautogui.typewrite("https://www.wikipedia.org/")
+    pyautogui.typewrite(["enter"])
+
+
+
+def searchWiki(title):
+    pyautogui.click(1120, 570)
+    pyautogui.typewrite(title)
+    pyautogui.typewrite(["enter"])
+
+
+def selectWeb():
+    pyautogui.click(554, 53)
+    pyautogui.hotkey('ctrl', 'c')
+    url1 = pyperclip.paste()
+    return url1
+
+
+def postion():
+    print(pyautogui.position())
