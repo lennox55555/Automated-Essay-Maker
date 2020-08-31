942f6626400c97097c8ae366bc6b72ba477e6aa1
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-08-31 13:55:01 -0700

    Update format.py

--- format.py
+++ format.py
@@ -1,4 +1,39 @@
 import scrapeModule
+import docxModule
+
 
 def test(tags):
-    print(tags)
+    print("done")
+
+
+def onePage(text, font1, fontSize):
+    essay = "is responsible for many significant events through the world, for an example;" + text
+    docxModule.addPara(essay, font1, fontSize)
+    return 1
+
+def twoPage():
+    return 2
+
+def threePage():
+    return 3
+
+def fourPage():
+    return 4
+
+def fivePage():
+    return 5
+
+def sixPage():
+    return 6
+
+def sevenPage():
+    return 7
+
+def eightPage():
+    return 8
+
+def ninePage():
+    return 9
+
+def tenPage():
+    return 10
