ab34d109690a35fd2110c27a48bc79950e2f6f4e
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-09-03 10:45:04 -0700

    Update format.py

--- format.py
+++ format.py
@@ -2,12 +2,21 @@ import scrapeModule
 import docxModule
 
 
-def test(tags):
-    print("done")
 
 
-def onePage(text, font1, fontSize):
-    essay = "is responsible for many significant events through the world, for an example;" + text
+def narrateDir(text, font1, fontSize, title, narrate, length):
+    if narrate == 1 & length == 1:
+        onePage(text, font1, fontSize, title)
+
+
+
+def onePage(text, font1, fontSize, title):
+
+    essay = "In today's society, almost every " + title + \
+            " supporter is familiar with one of these words, inhumane and irrespective. Although the ideology regaurding "\
+            + title + " is frequently changing, it is able to touch every social and economic class of our society. " \
+                      "While this may pose a minor problem compared to other significant events currently ongoing in" \
+                      " the world, in the future it may be a different case"
     docxModule.addPara(essay, font1, fontSize)
     return 1
 
