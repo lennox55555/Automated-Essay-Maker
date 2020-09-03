da37086583ef89154584374684d71d1f616c36ff
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2020-09-03 10:45:25 -0700

    Update scrapeModule.py

--- scrapeModule.py
+++ scrapeModule.py
@@ -3,35 +3,21 @@ from bs4 import BeautifulSoup
 import format
 
 
-def scrapeResearch(sourceAlpha, font1, fontSize, title):
+def scrapeResearch(sourceAlpha, font1, fontSize, title, narrate, length):
     # sourceA
     soupA = BeautifulSoup(sourceAlpha, 'lxml')
     tags = soupA.find_all('p')
     for txt in tags:
         innerText =  "\"" + txt.text + "\""
-        format.onePage(innerText, font1, fontSize, title)
-        #docxModule.addPara(innerText, font1, fontSize)
-        print(innerText)
+        format.narrateDir(innerText, font1, fontSize, title, narrate, length)
 
 
 
 
 
 
-def scrapeArgue(sourceAlpha, sourceBravo, font1, fontSize):
-    # sourceA
-    soupA = BeautifulSoup(sourceAlpha, 'lxml')
-    tags = soupA.find_all('p')
-    for txt in tags:
-        innerText = txt.text
-        docxModule.addPara(innerText, font1, fontSize)
 
-    #sourceB
-    soupB = BeautifulSoup(sourceBravo, 'lxml')
-    tags1 = soupB.find_all('p')
-    for txt1 in tags1:
-        innerText1 = txt1.text
-        docxModule.addPara(innerText1, font1, fontSize)
+
 
 
 
